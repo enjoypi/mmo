b658331724a9fc2726c383e3854162fdc2c4d84e
Fan Zhou <>
2018-08-24 23:27:20 +0800

    SleepRandMS

--- ext/time.go
+++ ext/time.go
@@ -1,9 +1,14 @@
 package ext
 
 import (
+	"math/rand"
 	"time"
 )
 
 func SleepUntil(ms int64) {
 	time.Sleep(time.Unix(ms/1000, ms%1000*int64(time.Millisecond)).Sub(time.Now()))
+}
+
+func SleepRandMS(min, max int64) {
+	time.Sleep(time.Duration(min+rand.Int63n(max-min)) * time.Millisecond)
 }
